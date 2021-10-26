bbfa67f8c5cd91c5e70681ec71a26c1acef3edbc
Sivolonsky <bibert.shmibert@gmail.com>
2021-10-26 15:54:43 +0400

    remove coverage dir

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,16 +7,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2, options) => {
-    if (options.format) {
-      const f1split = filepath1.split('.');
-      const f2split = filepath2.split('.');
-      const getFormat = `${f1split[1]}, ${f2split[1]}`;
-    }
+  .action((filepath1, filepath2) => {
+    const result = compare(filepath1, filepath2);
+	console.log(result);
+    return result;
   })
-
   .parse(process.argv);
-
 // const result1 = openFile('fixture1.json');
 // console.log(openFile('fixture2.json'));
 // console.log(JSON.parse(result1));
--- src/openfile.js
+++ src/openfile.js
@@ -1,14 +1,13 @@
 import { readFileSync } from 'fs';
-import { path, dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const openFile = (filename) => {
-  const getFixturePath = path.join(__dirname, '..', '__fixtures__', filename);
+  const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
   const readFile = readFileSync(getFixturePath(filename), 'utf8');
-  console.log(readFile('file1.json'));
   return readFile;
 };
 
